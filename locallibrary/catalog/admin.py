c2965dfe892eca8a9e7e8720acb6cece7af2b5a8
JL CHEN <cjunlang@gmail.com>
2019-07-13 16:17:16 +0800

    add admin views

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from catalog.models import Author,Book,Genre,BookInstance
 
-# Register your models here.
+# admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+admin.site.register(Genre)
+
+
+class Authoradmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name' , 'last_name', ('date_of_birth', 'date_of_death')]
+
+admin.site.register(Author,Authoradmin)
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            "fields": (
+                'book', 'imprint', 'id'
+            ),
+        }),
+        ('Acailability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
